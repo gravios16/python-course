c616b7d453f1af5465c351add338807423c6edfb
Massimo Ludeña <massimo.ludena.work@gmail.com>
2018-11-14 23:05:22 -0500

    TODO 2 Done

--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -42,4 +42,28 @@ def get_pet_labels(image_dir):
     """
     # Replace None with the results_dic dictionary that you created with this
     # function
-    return None
+
+    in_files = listdir(image_dir)
+    results_dic = dict()
+
+    for idx in range(0, len(in_files), 1):
+        if in_files[idx][0] != ".":
+
+            pet_label = ""
+
+            filename_slices = in_files[idx].lower().split("_")
+
+            for word in filename_slices:
+                if word.isalpha():
+                    pet_label += word + " "
+
+            pet_label = pet_label.strip()
+            
+            if in_files[idx] not in results_dic:
+               results_dic[in_files[idx]] = [pet_label]
+              
+            else:
+                print("** Warning: Duplicate files exist in directory:", 
+                    in_files[idx])
+
+    return results_dic
